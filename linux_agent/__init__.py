835c44ab114e4eae6008e4710611cb053a69d7d9
Darragh O'Reilly <doreilly@suse.com>
2026-02-19 15:34:12 +0000

    Set default model to gemini-2.5-flash

--- linux_agent/__init__.py
+++ linux_agent/__init__.py
@@ -2,7 +2,7 @@ import os
 
 MODEL = os.getenv("GOOGLE_GENAI_MODEL")
 if not MODEL:
-    MODEL = "gemini-2.0-flash"
+    MODEL = "gemini-2.5-flash"
 
 # MODEL needs to be defined before this import
 from . import agent  # pylint: disable=wrong-import-position
